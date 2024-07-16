1adcf66bec6636839e4c9f24d28062cdad0bd6af
Gazi2050 <gazi45344@gmail.com>
2024-07-16 18:17:49 +0600

    updateed webpack.config.js and README.md

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # zet.js
 zet.js is a flexible and easy full-stack JavaScript framework that simplifies web development with seamless frontend and backend integration, and more.
+
+```
+npx create-zet-app my-new-app
+
+```
--- webpack.config.js
+++ webpack.config.js
@@ -2,37 +2,37 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'production',
     entry: './src/frontend/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
-    },
-    resolve: {
-        extensions: ['.js', '.jsx'],
+        path: path.resolve(__dirname, 'dist'),
     },
     module: {
         rules: [
             {
-                test: /\.(js|jsx)$/,
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.jsx?$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react'],
+                    },
                 },
             },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
         ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: './public/index.html',
+            filename: 'index.html',
         }),
     ],
-    devServer: {
-        static: path.join(__dirname, 'public'),
-        compress: true,
-        port: 3000,
-    },
 };
